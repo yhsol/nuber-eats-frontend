919c7dc31078a9ad64f73c59146a61129fe8e0f2
yhsol <loshY244110@gmail.com>
2021-09-05 19:46:30 +0900

    feat: Apollo Client setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { ApolloProvider } from "@apollo/client";
+import { apolloClient } from "../apollo";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  console.log("apollo: ", apolloClient);
+  return (
+    <ApolloProvider client={apolloClient}>
+      <Component {...pageProps} />;
+    </ApolloProvider>
+  );
 }
 export default MyApp;
